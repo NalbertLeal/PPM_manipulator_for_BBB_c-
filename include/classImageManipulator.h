a236f444ab931e74da69e72c96a09383f7e014ca
nalbertg <nalbertg@outlook.com>
2016-10-08 16:23:40 -0300

    update

--- include/classImageManipulator.h
+++ include/classImageManipulator.h
@@ -12,13 +12,19 @@ class ImageManipulator {
     void imageWriter(std::string);
     void zoomIn(int);
     void zoomOut(int);                    //
-    void applyFilter(std::string);        // apply any filter to the image, receive the name of the filter that going to apply
-    void applyFilterNegative(void);       // apply the negative filter on the image
+    void applyBlur(void);
+    void applyBin(void);
+    void applNagative(void);
+    void applyBorder(void);
+    void applySharpe(void);
+    void applyGrey(void);
+    void applyNegative(void);
     void run(int, const char *); // the program star to run when call this method
   private:
     int linesNumber;                      // variable that store the number of lines (heigth) of a image
     int colunmNumber;                     // variable that store the number of columns (weigth) of a image
     int maxPixelValue;
+    std::string fileOutName;
     Vector<Vector<PixelBase>> imageOriginal;  // store each line of pixel info of the image in a index of a vector
     Vector<Vector<PixelBase>> imageMod;  // store each line of pixel info of the modified image in a index of a vector
 
--- src/classImageManipulator.cpp
+++ src/classImageManipulator.cpp
@@ -18,19 +18,111 @@ ImageManipulator::ImageManipulator() {
   this->maxPixelValue = 0;
 }
 
-void ImageManipulator::applyFilterNegative() {
-
-}
-
-void ImageManipulator::applyFilter() {
-
-}
-
 void ImageManipulator::zoomOut() {
 
 }
 
 void ImageManipulator::zoomIn() {
+
+}
+
+void ImageManipulator::applyBlur() {
+
+  for(int i = 0; i < this->linesNumber; i++) {
+      for(int j = 0; j < this->colunmNumber; j++) {
+          this->imageMod[i][j].red = 0;
+          this->imageMod[i][j].green = 0;
+          this->imageMod[i][j].blue = 0;
+      }
+  }
+
+  for(int i = 1; i < this->linesNumber - 1; i++) {
+      for(int j = 1; j < this->colunmNumber - 1; j++) {
+          this->imageMod[i][j].red += (this->imageOriginal[i-1][j-1].red * 0);
+          this->imageMod[i][j].green += (this->imageOriginal[i-1][j-1].green * 0);
+          this->imageMod[i][j].blue += (this->imageOriginal[i-1][j-1].blue * 0);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i-1][j].red * -1);
+          this->imageMod[i][j].green += (this->imageOriginal[i-1][j].green * -1);
+          this->imageMod[i][j].blue += (this->imageOriginal[i-1][j].blue * -1);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i-1][j+1].red * 0);
+          this->imageMod[i][j].green += (this->imageOriginal[i-1][j+1].green * 0);
+          this->imageMod[i][j].blue += (this->imageOriginal[i-1][j+1].blue * 0);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i][j-1].red) * -1;
+          this->imageMod[i][j].green += (this->imageOriginal[i][j-1].green * -1);
+          this->imageMod[i][j].blue += (this->imageOriginal[i][j-1].blue * -1);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i][j].red * 5);
+          this->imageMod[i][j].green += (this->imageOriginal[i][j].green * 5);
+          this->imageMod[i][j].blue += (this->imageOriginal[i][j].blue * 5);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i][j+1].red * -1);
+          this->imageMod[i][j].green += (this->imageOriginal[i][j+1].green * -1);
+          this->imageMod[i][j].blue += (this->imageOriginal[i][j+1].blue * -1);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i+1][j-1].red * 0);
+          this->imageMod[i][j].green += (this->imageOriginal[i+1][j-1].green * 0);
+          this->imageMod[i][j].blue += (this->imageOriginal[i+1][j-1].blue * 0);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i+1][j].red * -1);
+          this->imageMod[i][j].green += (this->imageOriginal[i+1][j].green * -1);
+          this->imageMod[i][j].blue += (this->imageOriginal[i+1][j].blue * -1);
+
+          this->imageMod[i][j].red += (this->imageOriginal[i+1][j+1].red * 0);
+          this->imageMod[i][j].green += (this->imageOriginal[i+1][j+1].green * 0);
+          this->imageMod[i][j].blue += (this->imageOriginal[i+1][j+1].blue * 0);
+
+          if(this->imageMod[i][j].red < 0){
+              this->imageMod[i][j].red = 0;
+          }
+
+          if(this->imageMod[i][j].green < 0){
+              this->imageMod[i][j].green = 0;
+          }
+
+          if(this->imageMod[i][j].blue < 0){
+              this->imageMod[i][j].blue = 0;
+          }
+
+          if(this->imageMod[i][j].red > this->maxPixelValue){
+              this->imageMod[i][j].red = this->maxPixelValue;
+          }
+
+          if(this->imageMod[i][j].green > this->maxPixelValue){
+              this->imageMod[i][j].green = this->maxPixelValue;
+          }
+
+          if(this->imageMod[i][j].blue > this->maxPixelValue){
+              this->imageMod[i][j].blue = this->maxPixelValue;
+          }
+
+      }
+  }
+}
+
+void ImageManipulator::applyBin() {
+
+}
+
+void ImageManipulator::applNagative() {
+
+}
+
+void ImageManipulator::applyBorder() {
+
+}
+
+void ImageManipulator::applySharpe() {
+
+}
+
+void ImageManipulator::applyGrey() {
+
+}
+
+void ImageManipulator::applyNegative() {
 
 }
 
@@ -118,16 +210,95 @@ void ImageManipulator::run(int argc, const char *argv) {
   this->imageReader();
   pid_t processId;
 
-  // int numberOfForks = 4;
-  // for(int counter = 0; counter < numberOfForks; counter++) {
-    processId = fork();
-  // }
+  while(true) {
+    printf("\nOque voce deseja fazer? \n");
+    printf( "+---------------------------------+\n"
+            "| (1)   binarizacao,               |\n"
+            "| (2)   blur,                      |\n"
+            "| (3)   shapering,                 |\n"
+            "| (4)   bordas,                    |\n"
+            "| (5)   tons de cinza,             |\n"
+            "| (6)   negativo,                  |\n"
+            "| (7)   zoom in,                   |\n"
+            "| (8)   zoom out,                  |\n"
+            "| (9)   rotacao anti-horario,      |\n"
+            "| (10)  rotacao horaria,           |\n"
+            "| (11)  sair e salvar              |\n"
+            "+---------------------------------+\n");
 
-  if(processId == 0) {  // chield process
-    this->imageWriter("image#2.ppm");
-  }
-  else {  // parent process
-    this->imageWriter("image#1.ppm");
+    printf("Digite o numero de um dos comandos acima: ");
+    scanf("%i", &acao);
+    printf("\n");
+
+    // pid_t arrayOfProcessId[4];
+    // int numberOfForks = 4;
+    // for(int counter = 0; counter < numberOfForks; counter++) {
+      processId = fork();
+    //   arrayOfProcessId[i] = processId;
+    // }
+
+    if(processId == 0) {  // chield process
+      this->imageWriter("image#2.ppm");
+    }
+    else {  // parent process
+      this->imageWriter("image#1.ppm");
+    }
+
+    switch(acao) {
+        case 1: // No caso do usuario querer binarizar a imagem.
+            processId = fork();
+            if(processId == 0) {  // chield process
+              this->
+              this->applyBin();
+              this->imageWriter(this->fileOutName);
+            }
+            else {  // parent process
+              int status;
+              waitpid(processId, &status, 0);
+              this->applyBin();
+              this->imageWriter(this->fileOutName);
+            }
+            break;
+        case 2: // No caso do usuario querer aplicar o blur na imagem.
+            this->applyBlur(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 3: // No caso do usuario querer aplicar um shaperring na imagem.
+            this->applySharpe(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 4: // No caso do usuario querer aplicas bordas na imagem.
+            this->applyBorder(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 5: // No caso do usuario querer deixar a imagem em tons de cinza.
+            this->applyGrey(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 6: // No caso do usuario querer deixar a imagem em negativo.
+            this->applyNegative(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 7: // No caso do usuario querer uma redu��o de duas vezes na imagem.
+            this->zoomIn(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 8: // No caso do usuario querer uma amplia��o de duas vezes na imagem.
+            this->zoomOut(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 9: // No caso do usuario querer uma rota��o de 90� no sentido anti-horio.
+            this->rotacaoAntihorario(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 10: // No caso do usuario querer uma rotacao de 90� no sentido horario.
+            this->rotacaoHorario(novoNomeImg);
+            lerPPM(novoNomeImg);
+            break;
+        case 11: // Caso o usuario deseje sair do programa essa sera o case usado.
+            return 0;
+            break;
+    }
   }
 }
 
